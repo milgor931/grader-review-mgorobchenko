588f707ebfd33f2d4bb88a38f9666ea2dcbbd077
pyrotato <88985409+pyrotato@users.noreply.github.com>
2023-03-02 10:27:17 -0800

    updated grade.sh

--- grade.sh
+++ grade.sh
@@ -4,7 +4,24 @@ rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
 
-cp student-submission/ListExamples.java ./
-javac -cp $CPATH *.java
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+if [[ -f ./student-submission/ListExamples.java ]]
+then
+    echo 'ListExamples.java found'
+else
+    echo 'ListExamples.java not found. Please try again.'
+    exit 1
+fi
 
+cp ./student-submission/ListExamples.java ./
+
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java 2>compile-err.txt
+
+if [[ $? -ne 0 ]]
+then
+    echo 'Error compiling. Please try again.'
+    exit 1
+fi
+
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > res.txt
+
+grep "Failures: " res.txt > failReport.txt
